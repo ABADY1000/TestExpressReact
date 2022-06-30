8d8b5817f50ef761f5e95d6bd82e0468b8c048bc
abady1000 <abady1000@gmail.com>
2022-06-30 15:20:56 +0300

    comment npm

--- run_project.sh
+++ run_project.sh
@@ -1,11 +1,13 @@
 cd client
-npm i
+# npm i
 npm run start &
 pid1=$!
+
 cd ../server
-npm i
+# npm i
 npm run start
 pid2=$!
+
 
 wait pid1 && echo "PID1:(${pid1}) exited normally"
 wait pid2 && echo "PID2:(${pid2}) exited normally"
